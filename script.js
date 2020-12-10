bbe2dc63f59e349b963cf6a9a0329787b91c4593
Jonah Lotegeluaki <25lotegj@elmbrookstudents.org>
2020-12-10 13:47:34 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function salute() {
+    alert("SALUTE THE FLAG OR DIE");
+}
+function salutealot() {
+    salute();
+    salute();
+    salute();
+    salute();
+    salute();
+    salute();
+    salute();
+    salute();
+    salute();
+    salute();
+    salute();
+}
+function poop() {
+    x = prompt("ANSWER OR DIE: WHAT IS YOUR NAME?");
+}
